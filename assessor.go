92dce82ee70273f595a159e2ca7e89b9ac6617b4
storyicon <storyicon@stroryicon>
2018-05-18 16:01:06 +0800

    update

---
+++ assessor.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"golang-proxy/library"
+	"log"
+	"math"
+	"strings"
+	"time"
+
+	"github.com/parnurzeal/gorequest"
+	"github.com/robfig/cron"
+)
+
+type Assessor struct {
+	Config    *library.Config
+	Queue     []*library.ValidProxy
+	MySQL     *library.MySQL
+	Scheduler *cron.Cron
+	Count     int
+}
+
+/**
+Set 4 impact factors, namely AssessTimes, SuccessTimes, Speed, Mutation
+Continuously increasing Mutation value will lead to a sharp drop in Score
+Formula affected by SuccessRate and AssessTimes at the same time.
+Formulas can be derived by yourself
+*/
+func GetScore(p *library.ValidProxy) float64 {
+	times := float64(p.AssessTimes)
+	success := float64(p.SuccessTimes)
+	speed := math.Sqrt(float64(library.ProxyAssessTimeOut)) / p.AvgResponseTime
+	mutation := 1 / math.Pow(float64(p.ContinuousFailedTimes)+1, 2.0)
+	return success * speed * mutation / math.Sqrt(times)
+}
+
+func (s *Assessor) ProxyAssess(p *library.ValidProxy) {
+	var r library.HTTPbinIP
+	request := gorequest.New().Proxy(p.Content).Timeout(library.ProxyAssessTimeOut * time.Second)
+	timeStart := time.Now().UnixNano() / 1e6
+	resp, _, errs := request.Get("http://httpbin.org/ip").
+		EndStruct(&r)
+	if len(errs) == 0 && resp.StatusCode == 200 && strings.Contains(p.Content, r.Origin) {
+		timeCost := time.Now().UnixNano()/1e6 - timeStart
+		s.ProxyAssessFeedBack(p, 1, float64(timeCost)/1e3)
+		log.Printf("Pass Assess(%dms): %s", timeCost, p.Content)
+	} else {
+		log.Printf("Fail Assess: %s", p.Content)
+		s.ProxyAssessFeedBack(p, 0, float64(library.ProxyAssessTimeOut)*1.5)
+	}
+	return
+}
+
+func (s *Assessor) ProxyAssessFeedBack(p *library.ValidProxy, isSucc int, responseTime float64) {
+	p.AssessTimes++
+	times := float64(p.AssessTimes)
+	p.AvgResponseTime = (p.AvgResponseTime*(times-1.0) + responseTime) / times
+
+	if isSucc == 1 {
+		p.ContinuousFailedTimes = 0
+	} else {
+		p.ContinuousFailedTimes++
+	}
+	p.SuccessTimes += isSucc
+	p.LastAssessTime = time.Now().Unix()
+	p.Score = GetScore(p)
+	s.UpdateValidProxy(p)
+}
+
+func (s *Assessor) UpdateValidProxy(p *library.ValidProxy) {
+	succRate := float64(p.SuccessTimes) / float64(p.AssessTimes)
+	if succRate < library.AllowProxyAssessSuccessRateMin {
+		s.MySQL.Connection.Delete(p)
+	} else {
+		s.MySQL.Connection.Save(p)
+	}
+}
+
+func (s *Assessor) GetValidProxy() {
+	length := library.ProxyAssessQueueMin - len(s.Queue)
+	//log.Printf("QueueLength:%d, Dert:%d", len(s.Queue), length)
+	if length > 0 {
+		s.MySQL.Connection.
+			Where("unix_timestamp(now()) - last_assess_time >= ?", library.ProxyAssessInterval).
+			Or("last_assess_time = ?", 0).
+			Order("last_assess_time").
+			Limit(length).
+			Find(&s.Queue)
+		// log.Printf("Maybe has fetched %d from database, current queue length: %d", len(s.Queue)-length, len(s.Queue))
+	}
+}
+
+func (s *Assessor) Start() {
+	s.GetValidProxy()
+	s.Scheduler.AddFunc("@every 3s", func() {
+		s.GetValidProxy()
+	})
+	s.Scheduler.Start()
+	s.Count = 0
+	var p *library.ValidProxy
+	for {
+		time.Sleep(50 * time.Millisecond)
+		if len(s.Queue) > 0 {
+			s.Count++
+			p, s.Queue = s.Queue[0], s.Queue[1:]
+			go s.ProxyAssess(p)
+		}
+		p = nil
+	}
+}
+
+func NewAssessor() *Assessor {
+	c := library.GetConfig()
+	d := library.GetMysqlDsn(c)
+	return &Assessor{
+		Config:    c,
+		MySQL:     library.NewMySQL(d),
+		Scheduler: cron.New(),
+	}
+}
+
+func main() {
+	// log.SetFlags(log.Lshortfile | log.LstdFlags)
+	assessor := NewAssessor()
+	assessor.Start()
+}
---
+++ publisher.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"golang-proxy/library"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/gocolly/colly"
+	nsq "github.com/nsqio/go-nsq"
+	"github.com/robfig/cron"
+)
+
+type Publisher struct {
+	Config      *library.Config
+	Source      *[]library.Source
+	NSQProducer *nsq.Producer
+	Scheduler   *cron.Cron
+}
+
+func (p *Publisher) NewCrawler(s *library.Source) {
+	c := colly.NewCollector(
+		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"),
+		colly.Async(true),
+	)
+	c.Limit(&colly.LimitRule{
+		Parallelism: s.Category.Parallelnumber,
+	})
+	c.OnRequest(func(r *colly.Request) {
+		if s.Debug {
+			log.Println("Start visit", r.URL)
+		}
+	})
+	c.OnHTML(s.Selector.Iterator, func(e *colly.HTMLElement) {
+		item := e.DOM
+		if s.Selector.Filter == "" || item.Find(s.Selector.Filter).Length() > 0 {
+			ip := item.Find(s.Selector.IP).Text()
+			if strings.Count(ip, ".") == 3 {
+				proxy := library.Proxy{
+					IP:        ip,
+					Port:      item.Find(s.Selector.Port).Text(),
+					Protocal:  strings.ToLower(item.Find(s.Selector.Protocal).Text()),
+					Proxytype: item.Find(s.Selector.Proxytype).Length(),
+				}
+				if proxy.Protocal == "" {
+					proxy.Protocal = "http"
+				}
+				ps, _ := library.JSONEncode(proxy)
+				p.NSQProducer.Publish(library.NSQTopic, ps)
+				if s.Debug {
+					log.Printf("[%s] Get a Proxy %s", s.Name, string(ps))
+				}
+			}
+		}
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		if s.Debug {
+			log.Println("Finished visit", r.Request.URL)
+		}
+	})
+
+	c.Visit(s.Page.Entry)
+
+	for i := s.Page.From; i <= s.Page.To; i++ {
+		library.SleepRangeTime(s.Category.DelayRange)
+		url := strings.Replace(s.Page.Template, "{page}", strconv.Itoa(i), -1)
+		c.Visit(url)
+	}
+}
+
+func NewPublisher() *Publisher {
+	c := library.GetConfig()
+	return &Publisher{
+		Config:      c,
+		Source:      library.GetSource(),
+		NSQProducer: library.NewNSQProduer(c.Nsq.ProducerAddr),
+		Scheduler:   cron.New(),
+	}
+}
+
+func (p *Publisher) Start() {
+	var s *library.Source
+	length := len(*p.Source)
+	log.Printf("total %d source was found", length)
+	for i := 0; i < length; i++ {
+		s = &(*p.Source)[i]
+		(func(pub *Publisher, sour *library.Source) {
+			go pub.NewCrawler(sour)
+			log.Printf("Periodical Task %s Was Assigned %s", sour.Name, sour.Category.Interval)
+			pub.Scheduler.AddFunc(sour.Category.Interval, func() {
+				pub.NewCrawler(sour)
+			})
+		})(p, s)
+	}
+	p.Scheduler.Start()
+	select {}
+}
+
+func main() {
+	publisher := NewPublisher()
+	publisher.Start()
+}
