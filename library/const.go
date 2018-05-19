4c40a2ec8fee78709c2548f4332febb9e6a0f92d
storyicon <475783185@qq.com>
2018-05-19 15:35:03 +0800

    update

--- library/const.go
+++ library/const.go
@@ -3,7 +3,7 @@ package library
 const (
 	NSQTopic                       = "proxy"
 	ProxyPreAssessTimes            = 1
-	ProxyAssessTimeOut             = 1
+	ProxyAssessTimeOut             = 3
 	ProxyAssessInterval            = 60
 	ProxyInitScore                 = 1
 	ProxyAssessQueueMin            = 1000
